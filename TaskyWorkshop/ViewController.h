5ef9e2cc0392a269956e6f0bc69d7bf08c3181b8
Macos <macos@Macoss-MacBook-Pro.local>
2025-04-23 09:41:57 +0200

    initial commit

---
+++ TaskyWorkshop/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TaskyWorkshop
+//
+//  Created by Macos on 23/04/2025.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaskyWorkshop/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TaskyWorkshop
+//
+//  Created by Macos on 23/04/2025.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
